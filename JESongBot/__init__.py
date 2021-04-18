50f75d3bcb8c8d7261b06de135688c4280352b04
@AmKuSaL <78016063+kusalCY@users.noreply.github.com>
2021-04-18 12:16:58 +0530

    Update __init__.py

--- JESongBot/__init__.py
+++ JESongBot/__init__.py
@@ -1,4 +1,4 @@
-# Infinity BOTs <https://t.me/Infinity_BOTs>
+# CyberHackers <https://t.me/Cyber0Hacker>
 
 import logging
 from pyrogram import Client
@@ -10,4 +10,4 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-Cyber VC bot = Client("CyberSongBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
+JESongBot = Client("JESongBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
